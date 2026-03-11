c858a1bc142dccca5b6e99be713415f236613558
nghiatm-twendee <nghiatm@twendeesoft.com>
2026-03-11 16:49:14 +0700

    feat: :mag: minor SEO update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute("/")({
 function RouteComponent() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center gap-2.25">
-      <h1 className="text-40px font-extrabold">WELCOME TO SERVICE ENTRY</h1>
+      <h1 className="text-40px font-extrabold">WELCOME TO FAIYAH.IO</h1>
       <p className="text-2xl font-normal">Your Gateway to DeFi Services</p>
     </div>
   );
