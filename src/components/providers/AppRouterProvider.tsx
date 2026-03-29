4a1ab9030a83466813029d53640edc1436a18810
nghiatm-twendee <nghiatm@twendeesoft.com>
2026-03-29 15:18:37 +0700

    fix: :adhesive_bandage: review fix

--- src/components/providers/AppRouterProvider.tsx
+++ src/components/providers/AppRouterProvider.tsx
@@ -1,12 +1,17 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "@/routeTree.gen";
+import { useEffect } from "react";
 
 const router = createRouter({ routeTree } as any);
 
 function AppRouterProvider() {
-  router.subscribe("onResolved", function resetToTop() {
-    window.scrollTo({ top: 0, behavior: "instant" });
-  });
+  useEffect(() => {
+    const unsubscribe = router.subscribe("onResolved", function resetToTop() {
+      window.scrollTo({ top: 0, behavior: "instant" });
+    });
+    return unsubscribe;
+  }, []);
+  
   return <RouterProvider router={router} />;
 }
 export default AppRouterProvider;
