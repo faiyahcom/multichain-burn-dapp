3ba58fa60c16cba793935d1e9be4d20c0fe6335e
nghiatm-twendee <nghiatm@twendeesoft.com>
2026-04-19 18:50:34 +0700

    fix: :bento: change img to reduce glow

--- src/components/common/glow/icon/stake-category-icon.tsx
+++ src/components/common/glow/icon/stake-category-icon.tsx
@@ -1,4 +1,3 @@
-import { IconStakeGlowCategory } from "@/assets/react";
 import { cn } from "@/lib/utils";
 
 interface Props {
@@ -8,7 +7,11 @@ interface Props {
 const StakeCategoryIcon: React.FC<Props> = ({ className }) => {
   return (
     <div className={cn("relative", className)}>
-      <IconStakeGlowCategory className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+      <img
+        src="/images/categories/stake-glow.png"
+        alt="stake glow icon"
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 scale-190"
+      />
     </div>
   );
 };
