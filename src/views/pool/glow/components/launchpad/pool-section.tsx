f39222882a9edc8b8919f9e7fefefeed459b031b
nghiatm-twendee <nghiatm@twendeesoft.com>
2026-05-08 17:20:29 +0700

    feat(launchpad): :sparkles: search UI

--- src/views/pool/glow/components/launchpad/pool-section.tsx
+++ src/views/pool/glow/components/launchpad/pool-section.tsx
@@ -1,10 +1,10 @@
-
+import LaunchpadSearch from "./search";
 
 const LaunchpadPoolSection = () => {
   return (
     <div className="space-y-3.75">
       <h2 className="text-28px font-semibold">LAUNCHPADS</h2>
-      
+      <LaunchpadSearch />
     </div>
   );
 };
